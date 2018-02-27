260597e32e5003a43075c8f9177c6e4984ae3e00
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2018-02-27 12:29:39 +0000

    ACP_MIGRATION: corrected network policy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 export WHITELIST=${WHITELIST:-0.0.0.0/0}
 
-if [ $ENVIRONMENT == "prod" ]
+if [ ${ENVIRONMENT} == "prod" ]
 then
     export KUBE_TOKEN=${PROD_KUBE_TOKEN}
     export DNS_PREFIX=
