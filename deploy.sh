421019d0dc0a7f81a106f7658ab2b092905f3923
Tim Hammonds <tim.hammonds@digital.homeoffice.gov.uk>
2018-02-28 13:39:34 +0000

    ACP_MIGRATION: few corrections

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+
 export KUBE_NAMESPACE=${KUBE_NAMESPACE}
 export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 export WHITELIST=${WHITELIST:-0.0.0.0/0}
 
-if [ ${ENVIRONMENT} == "prod" ]
+if [ "${ENVIRONMENT}" == "prod" ]
 then
     export KUBE_TOKEN=${PROD_KUBE_TOKEN}
     export DNS_PREFIX=
